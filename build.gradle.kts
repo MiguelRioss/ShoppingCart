2cfd3ac157b8c2efe95ba3ec434bf6649b4f52a8
Miguel Rios <a47244@alunos.isel.pt>
2026-09-24 15:58:31 +0100

    chore: consume renamed shipping provider library

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     testImplementation("org.apache.poi:poi:5.4.1")
     testImplementation("org.apache.poi:poi-ooxml:5.4.1")
     implementation("com.github.librepdf:openpdf:3.0.5")
-    implementation(files("libs/FedExApiMiddlwere-1.1.0-beta.6.jar"))
+    implementation(files("libs/ShippingApiProvider-1.1.0-beta.6.jar"))
 }
 
 kotlin {
